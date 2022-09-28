06828956ea0881c31e845b4e80ef53a46cf4c512
rutuj <rutuj@LAPTOP-HGULBHP5>
2022-09-28 12:26:50 +0530

    First Commit

---
+++ Day-1_ConnectingEclipsewithGitHub/src/org/tnsindia/basic/TestingEclipseForGitHUb.java
@@ -0,0 +1,11 @@
+package org.tnsindia.basic;
+
+public class TestingEclipseForGitHUb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to TNS India");
+	}
+	
+
+}
