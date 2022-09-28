0d64b87524d9028a096fc074ccc2a22fe9095bf9
rutuj <rutuj@LAPTOP-HGULBHP5>
2022-09-28 13:43:14 +0530

    Fourth Commit

--- Day-2IntroductionToJava/src/org/tnsindia/userinput/DemoOnInteger.java
+++ Day-2IntroductionToJava/src/org/tnsindia/userinput/DemoOnInteger.java
@@ -1,11 +1,13 @@
 package org.tnsindia.userinput;
 
 import java.util.Scanner;
+// program on user-input for Integer data-type
 
 public class DemoOnInteger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// for user-input
+		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the value of x: ");
 		int x = s.nextInt();
