b8244ee5434a9d44f8f34abb599119e98635cb2c
rutuj <rutuj@LAPTOP-HGULBHP5>
2022-09-28 13:10:57 +0530

    Second Commit

---
+++ Day-2IntroductionToJava/src/org/tnsindia/datatypes/DemoOnDatatypes.java
@@ -0,0 +1,19 @@
+package org.tnsindia.datatypes;
+
+public class DemoOnDatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 56;
+		short l = 5467;
+		byte a = 127;
+		char c = 'b';
+		String abc = "Welcome";
+		System.out.println(x);
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(l);
+		System.out.println(abc);
+	}
+
+}
