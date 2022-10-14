910df9c04ef690a24f00f111e85cfafee3c34e4b
rutuj <rutuj@LAPTOP-HGULBHP5>
2022-10-14 12:55:15 +0530

    Commit

---
+++ Assignment-3_Array/src/com/cg/array/TwoDimensionalArray.java
@@ -0,0 +1,11 @@
+package com.cg.array;
+
+public class TwoDimensionalArray 
+{
+	public static void main(String[] args) {
+		int arr[][]= {{11,3,5},{8,65,87}};
+		System.out.println(arr[0][2]);
+		System.out.println(arr[1][1]);
+
+	}
+}
