b6e5c5bccca71a55b38a4b61071b8c6705e2419d
rutuj <rutuj@LAPTOP-HGULBHP5>
2022-11-09 13:16:23 +0530

    Commit

---
+++ Day-22-GoShoppingApp/src/org/tnsindia/framework/NormalAcc.java
@@ -0,0 +1,22 @@
+package org.tnsindia.framework;
+
+public class NormalAcc extends ShopAcc {
+	private final float deliveryCharges;
+	public NormalAcc(int accNo, String accNm,float charges, float deliveryCharges) 
+	{
+		super(accNo, accNm,charges);
+		this.deliveryCharges = deliveryCharges;
+	}
+	public void bookProduct(float charges) {
+		System.out.println("Account No:" +this.getAccNo() +","+ "Account Name:" +this.getAccNm()+","+ "charges is: "+(charges+deliveryCharges));
+	}
+	
+	@Override
+	public String toString() {
+		return "NormalAcc [deliveryCharges=" + deliveryCharges + "]";
+	}
+	
+		
+	
+
+}
